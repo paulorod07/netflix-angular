0c39522f9d3c335f2ff48e260d0493dd6e0a2635
Paulo Rodrigues <pauloadesr@hotmail.com>
2020-05-02 16:47:10 -0300

    properties and trailers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  user: string = 'Paulo';
+
+  listTitles = [{
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 1',
+    relevance: 99,
+    age: 0,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  },
+  {
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 2',
+    relevance: 98,
+    age: 12,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  },
+  {
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 3',
+    relevance: 77,
+    age: 14,
+    parts: 2,
+    categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+  }];
+}
